ba2f16a3e6548bb58480c942540c5f409bbf1e14
Daniel Hollas <hollas@vscht.cz>
2015-03-20 17:50:56 +0100

    Fix bash string comparison in SetEnvironment.sh

--- bin/SetEnvironment.sh
+++ bin/SetEnvironment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# A simple script that should set the envirenmont for a specific program
-# for its most recent version.
+# A simple script that should set the environment
+# for a specific program to its most recent version.
+# It should work for all PHOTOX clusters.
 
 
 if [[ -z $1 ]];then
@@ -16,9 +17,9 @@ whoami=$(basename $0)
 
 # First, determine where we are. 
 # Currently works for as67-1 and a324 clusters.
-if [[ "$node" =~ s[0-9] ]] || [[ "$node" -eq "as67-1" ]];then
+if [[ "$node" =~ s[0-9] ]] || [[ "$node" = "as67-1" ]];then
    cluster=as67
-elif [[ "$node" =~ a[0-9] ]] || [[ "$node" -eq "403-a324-01" ]];then
+elif [[ "$node" =~ a[0-9] ]] || [[ "$node" = "403-a324-01" ]];then
    cluster=a324
 else
    echo "I did not recognize any of the PHOTOX clusters. Please check the script $0"
@@ -27,10 +28,10 @@ else
 fi
 
 #--MOLPRO--
-if [[ "$1" -eq "MOLPRO" ]];then
-if [[ $cluster -eq "as67" ]];then
+if [[ "$1" = "MOLPRO" ]];then
+if [[ $cluster = "as67" ]];then
    export  m12root=$(readlink -f /usr/local/programs/molpro/molpro2012.1/arch/amd64-intel_12.0.5.220/molpros_2012_1_Linux_x86_64_i8)
-elif [[ $cluster -eq "a324" ]] ;then
+elif [[ $cluster = "a324" ]] ;then
    export  m12root=$(readlink -f /usr/local/programs/common/molpro/molpro2012.1/arch/x86_64-intel_12.0.5.220/molpros_2012_1_Linux_x86_64_i8)
 fi
 MOLPROEXE=$m12root/bin/molpro
@@ -40,7 +41,7 @@ fi
 #--TeraChem--
 # Export only for nodes on a324
 # Not sure about this...The user might get confused.
-if [[ "$1" -eq "TERA" ]];then
+if [[ "$1" = "TERA" ]];then
    if [[ "$cluster" -ne "as67" ]] && [[ "$node" -ne "403-a324-01" ]] ;then
 
       if [ $node = "a25" ];then    #tesla node, we use older and faster version of Terachem
@@ -67,23 +68,23 @@ fi
 #-------------------------------------
 
 #--Gaussian--
-if [[ "$1" -eq "G09" ]];then
-if [[ $cluster -eq "as67" ]];then
+if [[ "$1" = "G09" ]];then
+if [[ $cluster = "as67" ]];then
    export g09root="/home/slavicek/G03/gaussian09/a02/g09"
-elif [[ $cluster -eq "a324" ]] ;then
+elif [[ $cluster = "a324" ]] ;then
    export g09root="/home/slavicek/G03/gaussian09/d01/arch/x86_64_sse4.2/g09"
 fi
 fi
 
 
 #--QCHEM
-if [[ "$1" -eq "QCHEM" ]];then
+if [[ "$1" = "QCHEM" ]];then
    export QC=/usr/local/programs/common/qchem/qchem-4.1/arch/x86_64
 fi
 
 
 #--CP2K--
-if [[ "$1" -eq "CP2K" ]];then
+if [[ "$1" = "CP2K" ]];then
 . /home/uhlig/intel/composer_xe_2013_sp1.4.211/bin/compilervars.sh intel64
 . /home/uhlig/intel/composer_xe_2013_sp1.4.211/mkl/bin/mklvars.sh intel64
 . /home/uhlig/build/libint/1.1.4-icc/env.sh
@@ -96,11 +97,11 @@ fi
 
 
 #--DFTB--
-if [[ "$1" -eq "DFTB" ]];then
+if [[ "$1" = "DFTB" ]];then
    export DFTBEXE=~hollas/bin/dftb+
 fi
 
-if [[ "$1" -eq "MOPAC" ]];then
+if [[ "$1" = "MOPAC" ]];then
    export MOPAC_LICENSE=/home/hollas/programes/MOPAC2012-CENTOS5
    export MOPACEXE=/home/hollas/programes/MOPAC2012-CENTOS5/MOPAC2012.exe
 fi
