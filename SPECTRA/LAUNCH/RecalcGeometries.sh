80c8b9974e039548e984ee78c628cff1d560155c
Stepan Srsen <srsen.stepan@gmail.com>
2023-11-09 15:46:36 +0100

    more options for geometries selection, streamlined comments and variables names

--- SPECTRA/LAUNCH/RecalcGeometries.sh
+++ SPECTRA/LAUNCH/RecalcGeometries.sh
@@ -1,99 +1,101 @@
 #!/bin/bash
 
-# This is a driver script which takes geometries
-# consecutively from xyz trajectory and launches calculations.
+# This is a driver script which takes geometries from xyz movie and launches calculations.
+# You can use PickGeoms.sh to filter out geometries before running this script.
+# EXAMPLE: if you have 10 geometries, and you want to skip the first 3 of them
+# and calculate the rest, set "imin=4" and "imax=0" (same as "imax=10" in this case)
 
-# You should use PickGeoms.sh to filter out geometries before running this script.
-
-# EXAMPLE: if you have 10 geometries, and you want to skip first 3 of them
-# and calculate the rest, set "firstgeom=4" and "lastgeom=0".
-# In this case, "lastgeom=0" will do the same as "lastgeom=10"
-
-######## SETUP ##########
-name=your_molecule      # name of the job
-firstgeom=1             # first geometry, will skip (first-1)geometries
-lastgeom=10             # last geometry, positive integer or 0 for all geometries up to the end of file
-movie=geometries.xyz    # file with xyz geometries
-program=GAUSS           # one of GAUSS, MOLPRO, QCHEM, ORCA
-jobs=1                  # determines number of jobs to submit
-                        # the calculations will be distributed accordingly
-nproc=1                 # number of processors per job
-                        # Be carefull, some programs (QCHEM) are a bit trickier to launch in parallel
-                        # You might need to modify line "$submit_path..."
+##### SETUP - basic options ######################################
+name=mol_name      # name of the job
+imin=1             # first geometry, will skip (imin-1) geometries
+imax=0             # last geometry, positive integer or 0 for all geometries up to the end of file
+movie=geoms.xyz    # file with xyz geometries
+program=GAUSS      # one of GAUSS, MNDO, MOLPRO, ORCA, QCHEM, TERA
+jobs=1             # number of jobs to submit, calculations will be distributed accordingly
+nproc=1            # number of processors per job
+                   # Some programs (e.g. QCHEM) are a bit trickier, you might need to modify $submit_path
 #submit="qsub -V -q aq -pe shm $nproc " # comment this line if you do not want to submit jobs automatically
-make_input="calc.$program.sh"  # script to make input files.
-submit_path="$program"    # script for launching a given program (GAUSS,TERA etc.)
-# Leave $version blank if you want the default version of a given program
-version=
-# Use script SetEnvironment.sh to determine available versions.
-######################
-
-
-if [[ ! -e $movie ]];then
-   echo "ERROR: File $movie does not exist."
+##### SETUP - rare options #######################################
+indices=           # file with indices (one per line) of geometries to recalculate instead from $imin to $imax
+                   # leave empty for using all geometries from imin to imax
+istep=1            # calculate every $istep-th geometry between $imin and $imax 
+make_input="calc.$program.sh"  # script to make input files
+submit_path="$program"         # script for launching a given program (GAUSS,TERA etc.)
+version=                       # leave blank if you want the default version of a given program
+                               # use script SetEnvironment.sh to determine available versions
+##################################################################
+# INITIALIZATION
+if [[ ! -f $movie ]] || [[ ! -r $movie ]];then
+   echo "ERROR: File \"$movie\" is not readable."
    exit 1
 fi
 
 natom=$(head -1 $movie | awk '{print $1}' )  # number of atoms
 let natom2=natom+2
-let natom1=natom+1
 
 lines=`cat $movie | wc -l` 
-geoms=`expr $lines / $natom2`
+ngeoms=`expr $lines / $natom2`
 
-if [[ "$lastgeom" -gt "$geoms" || $firstgeom -gt "$geoms" ]];then
-   echo "ERROR: Number of geometries ($geoms) is smaller than the requested number."
-   echo "Change parameter \"lastgeom\" or \"firstgeom\"."
-   exit 1
+if [[ -n $indices ]]; then
+   if [[ ! -f $indices ]] || [[ ! -r $indices ]]; then
+      echo "ERROR: The file \"$indices\" is not readable."
+      exit 1
+   fi
+   mapfile -t samples < $indices
+   echo "Using geometries from file \"$movie\"."
+   echo "Geometry indices loaded from file \"$indices\"."
+else
+   if [[ "$imax" -gt "$ngeoms" || $imin -gt "$ngeoms" ]];then
+      echo "ERROR: Number of geometries ($ngeoms) is smaller than the requested number."
+      echo "Change parameter \"imax\" or \"imin\"."
+      exit 1
+   fi
+   if [[ $imax -eq 0 ]];then
+      imax=$ngeoms
+   fi
+   samples=($(seq $imin $istep $imax))
+   echo "Using geometries $imin to $imax from file \"$movie\"."
 fi
 
-if [[ $lastgeom -eq 0 ]];then
-   lastgeom=$geoms
-fi
-
-let nsample=lastgeom-firstgeom+1
+let nsample=${#samples[@]}
+echo "Processing $nsample samples and generating $jobs job(s)..."
 
 if [[ $jobs -gt $nsample ]];then
    echo "WARNING: Number of jobs is bigger than number of samples."
    jobs=$nsample
 fi
 
+rname=r.$name.${samples[0]}
+
 # determine number of G09 calculations per job
 let injob=nsample/jobs
-#determine the remainder and distribute it evenly between jobs
+# determine the remainder and distribute it evenly between jobs
 let remainder=nsample-injob*jobs
 
-if [[ -e r.$name.$firstgeom.1 ]];then
-   echo "Error: it appears that you have already calculated geometry number $firstgeom."
+if [[ -e $rname.1 ]];then
+   echo "WARNING: it appears that you have already calculated geometry number $imin."
    echo "Should I proceed anyway? [yes/no]"
    read answer
-   if [[ "$answer" != "yes" ]];then
+   if [[ "$answer" != "yes" ]] && [[ "$answer" != "y" ]];then
       echo "ABORTING."
       exit 1
    fi
 fi
 
-rm -f r.$name.$firstgeom.*
-
+rm -f $rname.*
+##################################################################
+# CREATE INPUTS
 j=1
-let offset=(firstgeom-1)*natom2
-i=$firstgeom
-
-########################################################################
-
-while [[ $i -le $lastgeom ]]
+for i in "${samples[@]}"
 do
-   let offset=offset+natom2
-
-   head -$offset $movie | tail -$natom2 > temp.xyz
-
+   let offset=i*natom2
+   head -$offset $movie | tail -$natom2 > temp.xyz # TODO: use variable instead of temp file
    ./$make_input temp.xyz $name.$i.com $nproc
 
-   #DH warning, we are asuming here, that the second parameter is nproc
-   echo "$submit_path $name.$i.com $version " >>r.$name.$firstgeom.$j
+   #DH warning, we are assuming here, that the second parameter is nproc
+   echo "$submit_path $name.$i.com $version " >> $rname.$j
 
-
-#--Distribute calculations evenly between jobs for queue
+# Distribute calculations evenly between jobs for queue
    let curr_job++
    if [[ $remainder -le 0 ]];then
       let ncalc=injob
@@ -110,16 +112,14 @@ do
    let i++
 
 done
-########################################################################
-
-j=1
-
+rm -f temp.xyz
+##################################################################
 # SUBMIT JOBS
+j=1
 if [[ ! -z $submit ]];then
    while [[ $j -le $jobs ]]
    do
-      $submit -cwd -V r.$name.$firstgeom.$j
+      $submit -cwd -V $rname.$j
       let j++
    done
 fi
-
